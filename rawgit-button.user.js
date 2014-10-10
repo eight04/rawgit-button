c82728645877a21edc36439e16df645967acce23
eight <eight04@gmail.com>
2014-10-10 18:13:59 +0800

    Fix not replace bug caused by ajax loading.

--- rawgit-button.user.js
+++ rawgit-button.user.js
@@ -2,15 +2,31 @@
 // @name		Github Rawgit Button
 // @namespace	eight04.blogspot.com
 // @description	An userscript to add "Rawgit" button on github.
-// @include		https://github.com/*/blob/*
-// @version		1.0
+// @include		https://github.com/*
+// @version		1.1
 // @grant 		none
 // ==/UserScript==
 
 "use strict";
 
-var url = location.href.replace("github.com", "rawgit.com");
-url = url.replace("/blob/", "/");
-var btn = document.querySelector("#raw-url");
-btn.href = url;
-btn.textContent = "Rawgit";
+function replace(){
+	var btn = document.querySelector("#raw-url");
+	if (!btn || btn.classList.contains("rawgit")) {
+		return;
+	}
+	
+	var url = location.href.replace("github.com", "rawgit.com");
+	url = url.replace("/blob/", "/");
+	btn.href = url;
+	btn.classList.add("rawgit");
+	btn.textContent = "Rawgit";
+}
+
+var container = document.querySelector("#js-repo-pjax-container");
+if (container) {
+	new MutationObserver(function(e){
+		replace();
+	}).observe(container, {childList: true, subtree: true});
+}
+
+replace();
