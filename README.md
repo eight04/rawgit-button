e551a9074e3e0162ebebc20a5745a7150f5f3f54
eight <eight04@gmail.com>
2015-01-14 09:08:53 +0800

    Support github gist

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 rawgit-button
 =============
 
-An userscript to Add "Rawgit" button on github.
+An userscript to Add "[Rawgit][1]" button on github.
+
+[1]: https://rawgit.com/
+
+Changelog
+---------
+* Version 1.2.0 (Jan 14, 2015)
+	- Support github gist.
--- rawgit-button.user.js
+++ rawgit-button.user.js
@@ -3,32 +3,43 @@
 // @namespace	eight04.blogspot.com
 // @description	An userscript to add "Rawgit" button on github.
 // @include		https://github.com/*
-// @version		1.1
+// @include		https://gist.github.com/*
+// @version		1.2.0
 // @grant 		none
 // ==/UserScript==
 
 "use strict";
 
 function replace(){
-	var btn = document.querySelector("#raw-url");
+	// Check if raw-url button exists
+	var btn = document.querySelector("#raw-url") || document.querySelector(".raw-url");
 	if (!btn || btn.classList.contains("rawgit")) {
 		return;
 	}
-	
-	var url = location.href.replace("github.com", "rawgit.com");
-	url = url.replace("/blob/", "/");
-	
+
+	var url = location.href;
+	if (url.indexOf("gist.github.com") > -1) {
+		url = url.replace("gist.github.com", "rawgit.com");
+		url = url + "/raw/";
+	} else {
+		url = url.replace("github.com", "rawgit.com");
+		url = url.replace("/blob/", "/");
+	}
+
 	var newBtn = document.createElement("a");
 	newBtn.href = url;
 	newBtn.className = "minibutton";
 	newBtn.textContent = "Rawgit";
-	
+
 	btn.parentNode.insertBefore(newBtn, btn.nextSibling);
-	
+
 	btn.classList.add("rawgit");
 }
 
-var container = document.querySelector("#js-repo-pjax-container");
+var container =
+	document.querySelector("#js-repo-pjax-container") ||
+	document.querySelector("#js-pjax-container");
+
 if (container) {
 	new MutationObserver(function(e){
 		replace();
